4269373db6c154fd30b2ae42112449b6244292e7
Ramdaniofficial <ramdanistore01@gmail.com>
2022-03-21 09:59:08 +0700

    baseramdani

---
+++ result/lib/infobot.js
@@ -0,0 +1,16 @@
+const infobot = (prefix) => { 
+	return `
+• ----------[ *INFO BOT* ]----------
+• *NAMA BOT :* ${namabot}
+• *NOMOR BOT :* ${nomorbot}
+• *NAMA OWNER :* ${namaowner}
+• *NOMOR OWNER :* ${nomorowner}
+• *LINK :* wa.me/${nomorowner}
+• *CREATOR :* RAMDANI OFFICIAL
+• *BASE ORI :* RAMDANI OFFICIAL
+• *PREFIX :* MULTI PREFIX
+• *FITUR :* 300+ BARU BIKIN BANG
+• *NOTES :* JIKA ADA YG ERROR FIX SENDIRI, SOALNYA PAKE REST API GUA, JELEK NGAB:)
+`
+}
+exports.infobot = infobot
